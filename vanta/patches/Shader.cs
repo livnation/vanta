559cf068796412e1022341786cdeeb176cee413b
Liv <kantetnuser@proton.me>
2025-04-14 18:07:19 -0400

    Update Shader.cs
    remove faggot iidk color

--- vanta/patches/Shader.cs
+++ vanta/patches/Shader.cs
@@ -11,7 +11,6 @@ namespace vanta.patches
         private static void Postfix(GameObject __result)
         {
             __result.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
-            __result.GetComponent<Renderer>().material.color = new Color32(255, 128, 0, 128);
         }
     }
 }
